41503f09b2a0b236dcdeb9f1759f7fefe94510cc
xuanbao31 <baohxgbd210054@fpt.edu.vn>
2022-11-02 19:00:02 +0700

    update

--- ss13_Spring_sercurity/practice/src/main/java/com/security/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.security.repository;
-
-import com.security.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-    User findByUsername(String name);
-}
